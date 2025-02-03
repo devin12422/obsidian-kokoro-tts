2881d75e135f709cb16131f11bf7ec39a600158a
Devin Kramer <kramerdevin321@gmail.com>
2025-02-03 14:25:42 -0800

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,7 @@ import {ButtonComponent, PluginSettingTab, Setting} from "obsidian";
 import {TextInputPrompt} from "./TextInputPrompt";
 import TTSPlugin from "./main";
 import {LanguageVoiceModal} from "./LanguageVoiceModal";
+import {ServiceConfigurationModal } from "./ServiceConfigurationModal";
 
 export interface LanguageVoiceMap {
 	id: string;
